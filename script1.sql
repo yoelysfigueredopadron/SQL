8b447d2e204961d3e6ea3d880a71029727c064d7
Yoelys <yoelys1982@gmail.com>
2020-05-13 00:05:32 -0300

    Agregando comentarios

--- script1.sql
+++ script1.sql
@@ -1,23 +1,55 @@
+-- Creamos la base de datos test
 CREATE DATABASE test;
 
+-- Instanciamos la base de datos para que sea usada
 USE test;
 
+/* 
+	Creamos 5 tablas con sus claves primarias y foráneas que serán utilizadas para relacionarlas entre sí. 
+	Estas claves serán utilizadas a la hora de realizar las consultas para obtener la información de los
+	registros en cuestión.
+	
+	También creamos en cada una de ellas sus diferentes campos con sus tipos de datos correspondientes.
+	La información almacenada en el conjunto de todos los campos de	la tabla (la suma de todos ellos) nos dará
+	como resultado un registro o tupla de las tablas ('Vendedores', 'Sitios', 'Categorias', 'Articulos', 'Ventas').
+*/
+ 
+ 
+ /*
+	(vendedor_id) representa el campo (seller_id), los campos (nombre, apellidos) es información relacionada con cada vendedor.
+	(vendedor_id) es el campo de clave primaria que utilizaremos para relacional la tabla 'Vendedores' con la tabla 'Ventas'.
+ */
 CREATE TABLE Vendedores(
     vendedor_id INTEGER PRIMARY KEY,
     nombre VARCHAR(50),
     apellido VARCHAR(50)
 );
 
+/*	
+	(sitio_id) representa el campo (site_id) el mismo será utilizado para crear una relación entre la tabla 'Sitios'
+	y la tabla 'Ventas'. El campo nombre contiene el (nombre) de cada sitio web
+*/
 CREATE TABLE Sitios(
 	sitio_id VARCHAR(50) PRIMARY KEY,
 	nombre VARCHAR(50)
 );
 
+/*
+	(categoria_id) es el campo de clave primaria que será usado para crear una relación entre la tabla 'Categorias'
+	y la tabla 'Articulos'. El campo nombre contiene el (nombre) de cada categoría.
+*/
 CREATE TABLE Categorias(
 	categoria_id VARCHAR(50) PRIMARY KEY,
 	nombre VARCHAR(50)
 );
 
+/*
+	(articulo_id) es el campo de clave primaria que será usado para crear una relación entre la tabla 'Articulos'
+	y la tabla 'Ventas'. Los campos (titulos, costo) contienen información relacionada con el artículo en sí. Mientras
+	que 'categoria_id' es un campo definido como clave foránea que va hacer referencia al campo del mismo nombre en la tabla
+	'Categorias' creando una relación de varios a uno donde varios artículos pertenecen a una categoría y una categoría puede
+	contener varios artículos.
+*/
 CREATE TABLE Articulos(
 	articulo_id VARCHAR(50) PRIMARY KEY,
     titulo VARCHAR(50),
@@ -26,6 +58,11 @@ CREATE TABLE Articulos(
 	FOREIGN KEY (categoria_id) REFERENCES Categorias(categoria_id)
 );
 
+/*
+	(venta_id) es el campo de clave primaria de la tabla 'Ventas'. Dentro de está tabla tenemos otros campos como:
+	(vendedor_id, sitio_id, articulo_id) los cuales están creados con los mismos nombre en las tablas 'Vendedores',
+	'Sitios', 'Articulos' respectivamente y son usados como claves foráneas para crear las relaciones entre estás tablas.
+*/
 CREATE TABLE Ventas(
     venta_id VARCHAR(50) PRIMARY KEY,
     vendedor_id INTEGER,
@@ -36,13 +73,14 @@ CREATE TABLE Ventas(
 	FOREIGN KEY (articulo_id) REFERENCES Articulos(articulo_id)
 ); 
 
+-- Con INSERT INTO introducimos información en las tablas
 -- Insert Vendedores
 INSERT INTO Vendedores VALUES(179571324, 'Juan', 'Hernandez');
 INSERT INTO Vendedores VALUES(179571325, 'Pedro', 'Gonzales');
 INSERT INTO Vendedores VALUES(179571326, 'Julio', 'Rodiguez');
 
 -- Insert Sitios
-INSERT INTO Sitios VALUES('MLA', 'www.melian.com');
+INSERT INTO Sitios VALUES('MLA', 'www.mercadolibre.com');
 INSERT INTO Sitios VALUES('POD', 'www.podemos.com');
 INSERT INTO Sitios VALUES('JOY', 'www.joyas.com');
 
@@ -72,8 +110,17 @@ INSERT INTO Ventas VALUES('VTS5', 179571325, 'JOY', 'ART5');
 INSERT INTO Ventas VALUES('VTS6', 179571326, 'MLA', 'ART8');
 INSERT INTO Ventas VALUES('VTS7', 179571326, 'MLA', 'ART9');
 
-/*	Recorrer todos los ítems publicados mostrando los resultados "id" del ítem, 
-  "title" del item, "category_id" donde está publicado y "name" de la categoría.*/
+/*
+	Creamos una consulta con cláusulas JOINs donde unimos todas las tablas 'Vendedores', 'Sitios', 'Categorias',
+	'Articulos', 'Ventas' por sus campos de relación (claves primarias y foráneas) haciendo un filtro con la cláusula
+	WHERE donde (vd.vendedor_id = 179571326  AND st.sitio_id = 'MLA'). vt, vd, st, at, ct son nombres de alias para las
+	tablas 'Ventas', 'Vendedores', 'Sitios', 'Articulos' y 'Categorias' respectivamente. Finalmente si existen coincidencias
+	con el filtro realizado mostramos el resultado de la consulta a través de la sentencia 
+	SELECT at.articulo_id, at.titulo, ct.categoria_id, ct.nombre
+	
+	nota: He cambiado los nombres de los campos y recorrimos todos los ítems publicados por el seller_id = 179571326 del
+	site_id = "MLA" mostrando los resultados "id" del ítem, "title" del item, "category_id" donde está publicado y "name" de la categoría.
+*/
 SELECT at.articulo_id, at.titulo, ct.categoria_id, ct.nombre
 FROM Ventas vt
 JOIN Vendedores vd ON(vt.vendedor_id = vd.vendedor_id)
--- script2.sql
+++ script2.sql
@@ -1,18 +1,36 @@
-﻿CREATE DATABASE NewDB;
+﻿-- Creamos una nueva base de datos llamada NewDB
+CREATE DATABASE NewDB;
 
+-- La instanciamos
 USE NewDB;
 
+--  Creamos las diferentes tablas con sus campos y tipos de datos. También creamos sus claves primarias y foráneas para relacionarlas
+
+/*	
+	(carrier_id) es clave primaria de la tabla 'Carrier' donde hay dos campos más (nombre, capacity) en los cuales almacenamos
+	información referente a los Carriers o transportistas (Carrier A, Carrier B, Carrier C). El campo (carrier_id) es clave
+	foránea en la tabla 'Costos'
+*/
 CREATE TABLE Carrier(
     carrier_id INTEGER PRIMARY KEY,
     nombre VARCHAR(50),
     capacity INTEGER
 );
 
+/*
+	(zona_id) es clave primaria de la tabla 'Zonas'. Tenemos otro campo para especificar los nombres de las diferentes zonas.
+	(zona_id) es clave foránea en la tabla 'Envios'
+*/
 CREATE TABLE Zonas(
 	zona_id INTEGER PRIMARY KEY,
 	zona VARCHAR(50)
 );
 
+/*
+	(costo_id) es clave primaria de la tabla 'Costos'. Tenemos otros campos con los nombres de (carrier_id, zona_id, costo, tiempo_entrega).
+	La tabla 'Costos' contiene la información los (costos) por envió de cada carrier(carrier_id) por las diferentes zonas(zona_id) y su (tiempo_entrega).
+	(carrier_id, zona_id) son claves foráneas en las tablas 'Carrier' y 'Zonas' respectivamente. 
+*/
 CREATE TABLE Costos(
 	costo_id INTEGER PRIMARY KEY,
     carrier_id INTEGER NOT NULL,
@@ -23,6 +41,11 @@ CREATE TABLE Costos(
 	FOREIGN KEY (zona_id) REFERENCES Zonas(zona_id)
 );
 
+/*
+	(envio_id) es clave primaria de la tabla 'Envios'. Tenemos otros campos con los nombres de ('zona_id', 'mes', 'cantidad_envios')
+	Los registros de la tabla 'Envios' nos brinda el detalle de la (cantidad_envios) de envios por zona (zona_id) en cada (mes).
+	(zona_id) es clave foránea en la tabla 'Envios'
+*/
 CREATE TABLE Envios(
 	envio_id INTEGER PRIMARY KEY,
 	zona_id INTEGER NOT NULL,
@@ -31,6 +54,8 @@ CREATE TABLE Envios(
 	FOREIGN KEY (zona_id) REFERENCES Zonas(zona_id)
 );
  
+-- Introducimos los datos con el INSERT INTO en cada tabla para todos sus campos
+
 -- Insert Carrier
 INSERT INTO Carrier VALUES(1, 'Carrier A', 10000);
 INSERT INTO Carrier VALUES(2, 'Carrier B', 10000);
@@ -55,6 +80,15 @@ INSERT INTO Envios VALUES(1, 1, 1, 40000);
 INSERT INTO Envios VALUES(2, 2, 1, 50000);
 INSERT INTO Envios VALUES(3, 3, 1, 60000);
 
+/*
+	Creamos una consulta con cláusulas JOINs donde unimos las tablas 'Carrier', 'Costos' y 'Envios' donde Ca, Co y E son sus
+	alias respectivamente. Hacemos la unión de las tablas a través del campo carrier_id y zona_id donde (Ca.carrier_id = Co.carrier_id)
+	y (E.zona_id = Co.zona_id). Agrupamos los Carriers por su campo nombre y los mostramos como resultado de la consulta usando
+	la sentencia SELECT Ca.nombre "Name", SUM(Ca.capacity * Co.costo *(E.cantidad_envios/Ca.capacity)) "Envio"
+	donde mostramos el costos total de la cantidad de envió por cada (Carrier) a través de un campo calculado usando la fórmula
+	capacidad de envio del carrier(Ca.capacity) multiplicado por el costo del envió del carrier(Co.costo) multiplicado por el resultado
+	de la cantidad de envios de la tabla 'Envios' (E.cantidad_envios) divido entre la capacidad del carrier(Ca.capacity) 
+*/
 SELECT Ca.nombre "Name", SUM(Ca.capacity * Co.costo *(E.cantidad_envios/Ca.capacity)) "Envio"
 FROM Carrier Ca 
 JOIN Costos Co ON (Ca.carrier_id = Co.carrier_id)
